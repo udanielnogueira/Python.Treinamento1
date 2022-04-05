3c4ab3faa2a48d87ea725efdad0a932c62f57734
udanielnogueira <danielsilvangr@gmail.com>
2022-04-05 18:43:53 -0300

    Update file

---
+++ mundo-1/desafio-019.py
@@ -0,0 +1,23 @@
+'''
+Um professor quer
+sortear um dos seus
+quatro alunos para
+apagar o quadro. Faça
+um programa qua ajude
+ele, lendo o nome deles
+e escrevendo o nome
+do escolhido.
+'''
+
+import random
+
+n1 = str(input('Insira o nome do aluno: '))
+n2 = str(input('Insira o nome do aluno: '))
+n3 = str(input('Insira o nome do aluno: '))
+n4 = str(input('Insira o nome do aluno: '))
+
+lista = [n1, n2, n3, n4]
+
+x = random.choice(lista)
+
+print(f'Aluno(a) sorteado(a): {x}')
