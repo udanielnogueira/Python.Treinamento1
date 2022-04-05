cd32b952381001ea1a81f89cf0e7fa545fd7b406
udanielnogueira <danielsilvangr@gmail.com>
2022-04-05 12:37:48 -0300

    Update file

---
+++ mundo-1/4-1-import.py
@@ -0,0 +1,20 @@
+'''
+import math
+math.sqrt()
+
+from math import sqrt
+sqrt()
+
+math.sqrt() raiz quadrada
+math.ceil() arredonda pra cima
+math.floor() arredonda pra baixo
+math.trunc() truncamento
+'''
+
+from math import sqrt, trunc # ctrl + espaço (aparece todas as funções)
+
+n = int(input('Digite um número '))
+
+r = sqrt(n)
+
+print(f'Raiz quadrada: {trunc(r):.2f}') 
