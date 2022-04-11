92a85b7d81b4af49c5d69da544bd974a320f17dd
udanielnogueira <danielsilvangr@gmail.com>
2022-04-11 08:32:57 -0300

    Update file

---
+++ mundo-1/desafio-034.py
@@ -0,0 +1,25 @@
+'''
+Escreva um
+programa que
+pergunte o salário de
+um Funcionário e
+calcule o valor do seu
+aumento.
+Para salários
+superiores a
+RS1.250.00, calcule um
+aumento de 10%.
+Para os inferiores ou
+iguais, o aumento é da
+15%.
+'''
+
+s = float(input('Salário R$ '))
+
+if s > 1250:
+    a = s * 0.10
+else:
+    a = s * 0.15
+
+print(f'Aumento de R$ {a}')
+print(f'Novo salário R$ {s+a}')
---
+++ mundo-1/desafio-035.py
@@ -0,0 +1,25 @@
+'''
+Desenvolva um
+programa que leia o
+comprimento de três
+retas e diga ao usuário
+se elas podem ou não
+formar um triângulo.
+
+'''
+
+r1 = int(input('reta 1: '))
+r2 = int(input('reta 2: '))
+r3 = int(input('reta 3: '))
+
+'''
+Condição de existência de um triângulo
+
+A soma das medidas de dois dos lados
+sempre será maior que a terceira medida.
+'''
+
+if (r1+r2)>r3 and (r1+r3)>r2 and (r2+r3)>r1:
+    print('As retas formam um triângulo.')
+else:
+    print('As retas não formam um triângulo.')
