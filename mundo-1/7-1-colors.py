4d477cf6f34b8ddf87cdec774c007904f2bdf89f
udanielnogueira <danielsilvangr@gmail.com>
2022-04-12 11:00:42 -0300

    Update file

---
+++ mundo-1/7-1-colors.py
@@ -0,0 +1,22 @@
+'''
+Estilo
+
+0: nenhum
+1: negrito
+4: sublinhdo
+7: negativo
+
+3 para cor do texto
+4 para cor da tela
+
+30/40: branco
+31/41: vermelho
+32/42: verde
+33/43: amarelo
+34/44: azul
+35/45: roxo
+36/46: ciano
+37/47: cinza
+'''
+
+print('\033[32;44mHello, World!')
