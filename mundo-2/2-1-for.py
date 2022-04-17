209db541051c63bd190273a50e03d6c725cf2998
udanielnogueira <danielsilvangr@gmail.com>
2022-04-16 21:30:47 -0300

    Update file

---
+++ mundo-2/2-1-for.py
@@ -0,0 +1,15 @@
+# 0 1 2 3 4 exclui 5
+for i in range(5):
+    print('Oi')
+
+# 0 1 2 3 4 5 exclui 6
+for i in range(0, 6): 
+    print('Tchau')
+
+# pulando de 2 em 2
+for i in range(0, 10, 2):
+    print(i)
+
+# de 100 a 91 diminuindo 1
+for i in range(100, 90, -1):
+    print(i)
