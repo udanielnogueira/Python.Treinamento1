52f94cc049d0ce75d3afa33fbd66bafe51af21f6
udanielnogueira <danielsilvangr@gmail.com>
2022-04-17 22:15:07 -0300

    Update file

--- mundo-2/2-2-input.py
+++ mundo-2/2-2-input.py
@@ -4,10 +4,10 @@ n = int(input('n: '))
 for i in range(0, n):
     print(i)
 
+# contagem do usuário
+li = int(input('limite inferior: '))
+lf = int(input('limite superior: '))
+pa = int(input('passo: '))
 
-ini = int(input('início: '))
-fim = int(input('fim: '))
-pas = int(input('passo: '))
-
-for i in range(ini, fim, pas):
+for i in range(li, lf, pa):
     print(i)
