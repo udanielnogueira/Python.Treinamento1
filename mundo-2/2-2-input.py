7de304eea21bb6604d9f866f609767ee1b6c7dd6
udanielnogueira <danielsilvangr@gmail.com>
2022-04-17 22:12:15 -0300

    Update file

---
+++ mundo-2/2-2-input.py
@@ -0,0 +1,13 @@
+# contagem
+n = int(input('n: '))
+
+for i in range(0, n):
+    print(i)
+
+
+ini = int(input('início: '))
+fim = int(input('fim: '))
+pas = int(input('passo: '))
+
+for i in range(ini, fim, pas):
+    print(i)
