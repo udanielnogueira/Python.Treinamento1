c401edae5d7b65c1f07398f8794c73cd3bbe58e9
udanielnogueira <danielsilvangr@gmail.com>
2022-04-22 15:13:16 -0300

    Update file

--- mundo-2/desafio-064.py
+++ mundo-2/desafio-064.py
@@ -3,3 +3,15 @@ Crie um programa que leia vários números inteiros pelo teclado. O programa só
 o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma
 entre eles (desconsiderando o flag).
 '''
+
+numero = 0
+acumulador = 0
+contador = 0
+
+while numero != 999:
+    numero = int(input('n: '))
+    if numero != 999:
+        acumulador += numero 
+        contador += 1
+
+print(f'Foram digitados {contador} números\nSoma: {acumulador}')
--- mundo-2/desafio-065.py
+++ mundo-2/desafio-065.py
@@ -3,3 +3,30 @@ Crie um programa que leia vários números inteiros pelo teclado. No final da ex
 todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele
 quer ou não continuar a digitar valores.
 '''
+
+continuar = 'S'
+acumulador = 0
+contador = 0
+numero = 0
+media = 0
+maior = 0
+menor = 9*100
+while continuar == 'S':
+    numero = int(input('n: '))
+
+    acumulador += numero
+    contador += 1
+    media = acumulador/contador
+
+    if numero > maior:
+        maior = numero
+    if numero < menor:
+        menor = numero
+
+    continuar = input('Deseja continuar? [S/N]: ').upper()
+
+media = acumulador/contador
+
+print('média: ', media)
+print('maior: ', maior)
+print('menor: ', menor)
