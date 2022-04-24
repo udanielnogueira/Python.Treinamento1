9fea9735d81df8c25fd7eff99293c63e9a4c274d
udanielnogueira <danielsilvangr@gmail.com>
2022-04-24 16:46:53 -0300

    Update file

--- world-2/desafio-058.py
+++ world-2/desafio-058.py
@@ -7,26 +7,34 @@ palpites foram necessários para vencer.
 import random
 from time import sleep
 
+# Título do Jogo
+print('='*50)
+msg = 'Jogo da Adivinhação!'
+print(f'{msg:^50}')
+print('='*50)
+
 acertou = False
 tentativas = 0
 
-while acertou == False: # outra forma: while not acertou
-    x = random.randint(0, 10)
+# Gerando escolha do computador
+x = random.randint(0, 10)
+
+while acertou == False:
+   
     n = int(input('Tente advinhar um número de 0 a 10: '))
 
-    '''
     print('Processando...')
     sleep(0.5)
-    '''
-
+    
     if n > 10:
-        print('Opção inválida')
+        print('\nOpção inválida')
     elif n == x:
-        print(f'Pensei no número {x}, acertou!')
+        print(f'\nPensei no número {x}, acertou!')
         acertou = True
         tentativas += 1
     else:
-        print(f'Pensei no número {x}, errou!')
+        print(f'\nErrou, tente mais uma vez.')
         tentativas += 1
 
-print(f'Número de tentativas: {tentativas}')
+# Score final
+print(f'\nNúmero de tentativas: {tentativas}')
