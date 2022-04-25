3783af84813d9fa785eee61180793a15754c79b3
udanielnogueira <danielsilvangr@gmail.com>
2022-04-25 09:06:23 -0300

    Update file

---
+++ world-3/1-4-types.py
@@ -0,0 +1,10 @@
+''' 
+Tuplas são imutáveis, para informações que não sofrerão modificação.
+https://blog.betrybe.com/tecnologia/tuplas-em-python/
+'''
+
+# aceita tipos diferentes
+pessoa = ('Daniel', 26, 'M', 54.5)
+
+for dados in pessoa:
+    print(dados)
