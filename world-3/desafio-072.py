9fe6db17ed7000031030e9b6ce2a2aaa9b6c4c97
udanielnogueira <danielsilvangr@gmail.com>
2022-05-31 14:09:18 -0300

    Update file

--- world-3/desafio-072.py
+++ world-3/desafio-072.py
@@ -8,6 +8,9 @@ Seu programa deverá ler um número pelo teclado
 
 numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
-x = int(input('Número de 0 a 20: '))
-
-print(f'O número {x} por extenso é {numeros[x]}!')
+while True:
+    x = int(input('Número entre 0 a 20: '))
+    
+    if 0 <= x <= 20:
+        print(f'O número {x} por extenso é {numeros[x]}!')
+        break
---
+++ world-3/desafio-073.py
@@ -0,0 +1,31 @@
+'''
+Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de
+Futebol, na ordem da colocação. Depois mostre:
+
+a) Apenas os 5 primeiros colocados.
+
+b) Os 4 últimos colocados.
+
+c) Uma lista em ordem alfabética.
+
+d) Em que posição está o time da Chapecoense.
+'''
+
+times = ('Palmeiras', 'Atlético MG', 'Corinthians', 'Coritiba', 'São Paulo', 'Athletico PR', 'Botafogo', 'Flamengo', 'Santos', 'AméricaMG', 'Fluminense', 'Internacional', 'Avaí', 'Bragantino', 'Goiás', 'Cuiabá', 'Atlético GO', 'Juventude', 'Ceará', 'Fortaleza')
+
+for i in range(0,5):
+    print(f'{i+1}º colocado: {times[i]}')
+
+print()
+
+for i in range(16,20):
+    print(f'{i+1}º colocado: {times[i]}')
+
+print()
+
+print('Times em ordem alfabética:', sorted(times))
+
+print()
+
+p = times.index('Flamengo')
+print(f'Flamengo está na {p+1}º posição')
