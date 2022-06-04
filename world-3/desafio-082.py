a1ad9f35af605ceef098c6a43422183378b248c6
udanielnogueira <danielsilvangr@gmail.com>
2022-06-04 16:58:23 -0300

    Update file

---
+++ world-3/desafio-082.py
@@ -0,0 +1,29 @@
+'''
+Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, 
+crie duas listas extras que vão conter apenas os valores pares e os valors ímpares 
+digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
+'''
+
+valores = []
+
+while True:
+    valores.append(int(input('n: ')))
+
+    continuar = str(input('Deseja continuar? [S/N] ')).upper()
+    if continuar == 'N':
+        break
+    else:
+        continue
+
+pares = []
+impares = []
+
+for valor in valores:
+    if valor % 2 == 0:
+        pares.append(valor)
+    else:
+        impares.append(valor)
+
+print(valores)
+print(pares)
+print(impares)
